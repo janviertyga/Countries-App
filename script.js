d436bb49ef5753bdea6d652c7d0f6b18e2b6bbbb
janviertyga <137413343+janviertyga@users.noreply.github.com>
2023-08-15 10:43:18 +0200

    My First commit

--- script.js
+++ script.js
@@ -11,12 +11,22 @@ document.addEventListener("DOMContentLoaded", () => {
                 const countryName = document.createElement("h2");
                 countryName.textContent = country.name.common;
 
+                const countryFlag = document.createElement("img");
+                countryFlag.src = country.flags.png;
+                countryFlag.alt = `${country.name.common} Flag`;
+                countryFlag.classList.add("country-flag");
+
                 const countryCode = document.createElement("p");
                 countryCode.textContent = `Country Code: ${country.cca2}`;
 
+                const countryPopulation = document.createElement("p");
+                countryPopulation.textContent = `Population: ${country.population}`;
+
+                countryCard.appendChild(countryFlag);
                 countryCard.appendChild(countryName);
                 countryCard.appendChild(countryCode);
-                
+                countryCard.appendChild(countryPopulation);
+
                 countriesContainer.appendChild(countryCard);
             });
         })
@@ -24,4 +34,5 @@ document.addEventListener("DOMContentLoaded", () => {
             console.error("Error fetching countries:", error)
         );
 });
+
 
